e4fd0ae75ca2174d161151c0cc7b79deacb5bc74
Elton Santos <eltonaxl@hotmail.com>
2021-10-12 19:04:10 -0300

    Add course

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  resources :courses
   get 'static_pages/landing_page'
   get 'static_pages/privacy_policy'
+
+  root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
